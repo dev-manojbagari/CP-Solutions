3aa6ba35d144575925d9235ab3c7cc692b87809e
manoj bagari <107521004+dev-manojbagari@users.noreply.github.com>
2023-08-27 09:09:35 +0530

    Committing files

---
+++ CodeForces/Java 8/110A | Nearly Lucky Number/220631301.java
@@ -0,0 +1,40 @@
+// Problem: A. Nearly Lucky Number
+// Contest: Codeforces - Codeforces Beta Round 84 (Div. 2 Only)
+// URL: https://codeforces.com/problemset/problem/110/A
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner reader  = new Scanner(System.in);
+        long num = reader.nextLong();
+        boolean allLucky=false;
+        int luckyCount =0;
+        
+        while(num>0){
+         long lastDigit = num%10;
+         
+         if(lastDigit==4||lastDigit==7)
+          luckyCount++;
+         else
+          allLucky = false;
+          
+         num = num/10;
+          
+        }
+        
+        if(allLucky||luckyCount==4||luckyCount==7){
+         System.out.println("YES");
+        }else
+         System.out.println("NO");
+        
+        
+        
+        
+    }
+    
+    // Other methods here
+}
